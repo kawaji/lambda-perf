2d6952b48a7348fbc41ead8dd344e89774ea8553
kawaji <kawaji.scratch@gmail.com>
2024-06-17 05:00:29 +0900

    prevent cache

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ const dataManager = {
 
 const load = async (dataManager) => {
   const request = await fetch(
-    "https://raw.githubusercontent.com/kawaji/lambda-perf/main/data/last.json?0.256892491199298"
+    "https://raw.githubusercontent.com/kawaji/lambda-perf/main/data/last.json?0.6086948009184032"
   );
   const json = await request.json();
   dataManager.fetchData = json;
